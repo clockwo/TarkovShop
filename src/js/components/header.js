477f4e6d7a8aa39646a2d2ab67910186a196eb54
clockwo <riddlexfors@gmail.com>
2023-09-24 12:34:17 +0300

    refactor: :recycle: Update create element functions

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,21 +3,19 @@ import { createElement } from "../utilities/utils.js";
 export const getHeaderElement = () => {
   const pagesNames = ["Home", "Shop", "Contact"];
 
-  const headerElement = createElement("header");
-  headerElement.classList.add("header");
+  const headerElement = createElement("header", { class: "header" });
 
-  const navElement = createElement("nav");
-  navElement.classList.add("nav", "wrapper");
+  const navElement = createElement("nav", { class: "nav wrapper" });
 
-  const ulElement = createElement("ul");
-  ulElement.classList.add("nav-list");
+  const ulElement = createElement("ul", { class: "nav-list" });
   ulElement.setAttribute("data-js-nav", "");
 
   for (const liName of pagesNames) {
-    const liElement = document.createElement("li");
-    liElement.classList.add("nav-item");
-    liElement.setAttribute("data-page", `${liName}`);
-    liElement.textContent = liName;
+    const liElement = createElement(
+      "li",
+      { class: "nav-item", "data-page": liName },
+      { textContent: liName },
+    );
     ulElement.appendChild(liElement);
   }
 
